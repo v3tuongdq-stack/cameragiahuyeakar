e621bf9137285672c15fdf30a66fa08e2082ba6b
v3tuongdq-stack <v3tuongdq@gmail.com>
2026-07-31 09:03:15 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const products = [
+  {
+    id: 1,
+    name: "Bộ Phát WiFi TP-Link",
+    price: "450.000đ",
+    image: "images/wifi.jpg"
+  },
+  {
+    id: 2,
+    name: "Camera Imou Outdoor 1",
+    price: "850.000đ",
+    image: "images/cam1.jpg"
+  },
+  {
+    id: 3,
+    name: "Camera Ezviz Indoor 2",
+    price: "590.000đ",
+    image: "images/cam2.jpg"
+  }
+  // Các cam3, cam4, cam5, cam6 tương tự...
+];
